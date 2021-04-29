ec6e6871dcaabcbc02f1c62bd9f4f0c461959819
blam <ben@blam.sh>
2021-04-29 13:57:00 +0200

    reworking again

--- index.js
+++ index.js
@@ -7,35 +7,26 @@ const open = require("open");
 program.version("0.0.1");
 
 const client = new Octokit({
-  auth: process.env.GITHUB_ACCESS_TOKEN,
+  auth: process.env.GITHUB_ACCESS_TOKEN || process.env.GITHUB_TOKEN,
 });
 
 program
   .command("issues")
   .option("-d, --days <days>", "amount of days previous", 3)
-  .option('--ignore-stale', 'ignore stale')
-  .action(async ({ days, ignoreStale }) => {
+  .action(async ({ days }) => {
     const date = Luxon.DateTime.local();
     const since = date.minus({ days }).toISO();
 
     const issues = await client.paginate(client.issues.listForRepo, {
-      owner: "spotify",
+      owner: "backstage",
       repo: "backstage",
       since,
-      per_page: 200,
+      per_page: 100,
     });
-  
-    const issuesToOpen = issues.filter((i) => {
-      if (ignoreStale) {
-        return !i.labels.some(l => l.name === 'stale')
-      }
-      return true;
-    })
 
-    console.log(issuesToOpen[issuesToOpen.length - 1].labels);
-    const issueUrls = issuesToOpen.map(({ html_url }) => html_url);
+    const issueUrls = issues.map(({ html_url }) => html_url);
 
-    // issueUrls.forEach((u) => open(u));
+    issueUrls.forEach((u) => open(u));
   });
 
 program.parse(process.argv);
