b193b4dac6547dbf41446d00911b58bd5ad663da
Fredrik Adelöw <freben@gmail.com>
2022-06-21 14:39:48 +0200

    add label support
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
-const { Octokit } = require("@octokit/rest");
-const Luxon = require("luxon");
-const { Command } = require("commander");
+const { Octokit } = require('@octokit/rest');
+const Luxon = require('luxon');
+const { Command } = require('commander');
 const program = new Command();
-const open = require("open");
-program.version("0.0.1");
+const open = require('open');
+program.version('0.0.1');
 
 const client = new Octokit({
   auth: process.env.GITHUB_ACCESS_TOKEN || process.env.GITHUB_TOKEN,
 });
 
 program
-  .command("issues")
-  .option("-d, --days <days>", "amount of days previous", 3)
-  .option("-s, --since <20210429T14:00>", "")
-  .action(async ({ days, since }) => {
+  .command('issues')
+  .option('-d, --days <days>', 'amount of days previous', 3)
+  .option('-s, --since <20210429T14:00>', '')
+  .option('-l, --labels <comma separated list of labels>', '')
+  .action(async ({ days, since, labels }) => {
     let timestamp;
     if (since) {
       timestamp = Luxon.DateTime.fromISO(since).toISO();
@@ -24,9 +25,10 @@ program
     }
 
     const issues = await client.paginate(client.issues.listForRepo, {
-      owner: "backstage",
-      repo: "backstage",
+      owner: 'backstage',
+      repo: 'backstage',
       since: timestamp,
+      ...(labels ? { labels } : {}),
       per_page: 100,
     });
 
