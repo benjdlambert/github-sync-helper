e191ae8d1d6183415d3bf76b1398474e83288690
blam <ben@blam.sh>
2024-05-08 11:08:05 +0200

    feat: support for other repos

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Luxon = require('luxon');
 const { Command } = require('commander');
 const program = new Command();
 const open = require('open');
+const chalk = require('chalk');
 program.version('0.0.1');
 
 const client = new Octokit({
@@ -15,8 +16,9 @@ program
   .option('-d, --days <days>', 'amount of days previous', 3)
   .option('-s, --since <20210429T14:00>', '')
   .option('-l, --labels <comma separated list of labels>', '')
+  .option('--repo <repo>', 'repo to search', 'backstage/backstage')
   .option('--include-prs', 'include pull requests', false)
-  .action(async ({ days, since, labels, includePrs }) => {
+  .action(async ({ days, since, labels, includePrs, repo: repoSlug }) => {
     let timestamp;
     if (since) {
       timestamp = Luxon.DateTime.fromISO(since).toISO();
@@ -25,13 +27,19 @@ program
       timestamp = date.minus({ days }).toISO();
     }
 
+    console.log(chalk.yellow`Pulling issues since ${timestamp}`);
+    console.log(chalk.yellow`Using repository: ${repoSlug}`);
+
+    const [owner, repo] = repoSlug.split('/');
     const issues = await client.paginate(client.issues.listForRepo, {
-      owner: 'backstage',
-      repo: 'backstage',
+      owner,
+      repo,
       since: timestamp,
       ...(labels ? { labels } : {}),
       per_page: 100,
     });
+
+    console.log(chalk.green`Found ${issues.length} issues`);
 
     for (const issue of issues) {
       if (issue.pull_request) {
